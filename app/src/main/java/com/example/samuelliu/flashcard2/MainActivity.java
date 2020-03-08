579918b1c58ba5d3d6b977dceea692db88bbaa01
kimberly-kao <38386920+kimberly-kao@users.noreply.github.com>
2020-03-08 12:21:27 -0700

    the flashcard app now save prgoress even after the app has been shut down

--- app/src/main/java/com/example/samuelliu/flashcard2/MainActivity.java
+++ app/src/main/java/com/example/samuelliu/flashcard2/MainActivity.java
@@ -8,12 +8,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
+    FlashcardDatabase flashcardDatabase;
+    int currentCardDisplayedIndex = 0;
+    List<Flashcard> allFlashcards;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        flashcardDatabase = new FlashcardDatabase(this);
+        allFlashcards = flashcardDatabase.getAllCards();
+        if (allFlashcards != null && allFlashcards.size() > 0) {
+            ((TextView) findViewById(R.id.flashcard_questions)).setText(allFlashcards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.theEgg)).setText(allFlashcards.get(0).getAnswer());
+            ((TextView) findViewById(R.id.theChicken)).setText(allFlashcards.get(0).getWrongAnswer1());
+            ((TextView) findViewById(R.id.neither)).setText(allFlashcards.get(0).getWrongAnswer2());
+        }
         findViewById(R.id.theEgg).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,9 +55,9 @@ public class MainActivity extends AppCompatActivity {
 
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
-                MainActivity.this.startActivityForResult(intent,100);
+                MainActivity.this.startActivityForResult(intent, 100);
             }
         });
 
@@ -76,24 +88,48 @@ public class MainActivity extends AppCompatActivity {
 
         findViewById(R.id.edit).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
-                String string1 = ((TextView)findViewById(R.id.flashcard_questions)).getText().toString();
-                String string2 = ((TextView)findViewById(R.id.theEgg)).getText().toString();
-                String string3 = ((TextView)findViewById(R.id.theChicken)).getText().toString();
-                String string4 = ((TextView)findViewById(R.id.neither)).getText().toString();
+                String string1 = ((TextView) findViewById(R.id.flashcard_questions)).getText().toString();
+                String string2 = ((TextView) findViewById(R.id.theEgg)).getText().toString();
+                String string3 = ((TextView) findViewById(R.id.theChicken)).getText().toString();
+                String string4 = ((TextView) findViewById(R.id.neither)).getText().toString();
                 intent.putExtra("stringKey1", string1);
                 intent.putExtra("stringKey2", string2);
                 intent.putExtra("stringKey3", string3);
                 intent.putExtra("stringKey4", string4);
-                MainActivity.this.startActivityForResult(intent,100);
+                MainActivity.this.startActivityForResult(intent, 100);
 
             }
         });
 
-    }
+        findViewById(R.id.next_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentCardDisplayedIndex++;
+                if (currentCardDisplayedIndex > allFlashcards.size()-1) {
+                    currentCardDisplayedIndex = 0;
+                }
+                ((TextView) findViewById(R.id.flashcard_questions)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                ((TextView) findViewById(R.id.theEgg)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+                ((TextView) findViewById(R.id.theChicken)).setText(allFlashcards.get(currentCardDisplayedIndex).getWrongAnswer1());
+                ((TextView) findViewById(R.id.neither)).setText(allFlashcards.get(currentCardDisplayedIndex).getWrongAnswer2());
+                findViewById(R.id.theChicken).setBackgroundColor(Color.parseColor("#C5F2FF"));
+                findViewById(R.id.theEgg).setBackgroundColor(Color.parseColor("#C5F2FF"));
+                findViewById(R.id.neither).setBackgroundColor(Color.parseColor("#C5F2FF"));
+            }
+        });
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        findViewById(R.id.delete).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardDatabase.deleteCard(((TextView) findViewById(R.id.flashcard_questions)).getText().toString());
+            }
+        });
+
+
+    }
+    protected void onActivityResult(int requestCode,int resultCode, Intent data){
         if(requestCode == 100 && data!= null){
             String string1 = data.getExtras().getString("String 1");
             String string2 = data.getExtras().getString("String 2");
@@ -104,9 +140,11 @@ public class MainActivity extends AppCompatActivity {
             ((TextView)findViewById(R.id.theChicken)).setText(string3);
             ((TextView)findViewById(R.id.neither)).setText(string4);
             Snackbar.make((findViewById(R.id.flashcard_questions)),"Card created!",Snackbar.LENGTH_SHORT).show();
+            flashcardDatabase.insertCard(new Flashcard(string1, string2, string3, string4));
         }
-
+        allFlashcards = flashcardDatabase.getAllCards();
     }
+
 
 
 }
